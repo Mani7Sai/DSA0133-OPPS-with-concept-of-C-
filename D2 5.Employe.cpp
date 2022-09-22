927123ef1bf77322b8fd5d1df1a007be173eaf4e
Mani7Sai <112700177+Mani7Sai@users.noreply.github.com>
2022-09-22 14:43:46 +0530

    Update D2 5.Employe.cpp

--- D2 5.Employe.cpp
+++ D2 5.Employe.cpp
@@ -7,15 +7,11 @@ class employee
 {
 	int   emp_number;
 	char  emp_name[20];
-	float emp_basic;
-	float emp_da;
-	float emp_it;
-	float emp_net_sal;
-
+	char emp_dept_name[20];
+	char emp_designation[20];
 	public:
 
 		void get_emp_details();
-		float find_net_salary(float basic, float da, float it);
 		void show_emp_details();
 };
 
@@ -25,28 +21,18 @@ void employee :: get_emp_details()
 	cin>>emp_number;
 	cout<<"\nEnter employee name: ";
 	cin>>emp_name;
-	cout<<"\nEnter employee basic: ";
-	cin>>emp_basic;
-	cout<<"\nEnter employee DA: ";
-	cin>>emp_da;
-	cout<<"\nEnter employee IT: ";
-	cin>>emp_it;
+	cout<<"\nEnter employee dept name: ";
+	cin>>emp_dept_name;
+	cout<<"\nEnter employee designation: ";
+	cin>>emp_designation;
 }
-
-float employee :: find_net_salary(float basic, float da, float it)
-{
-    return (basic+da)-it;
-}
-
 void employee :: show_emp_details()
 {
-	cout<<"\n\n**** Details of  Employee ****";
-	cout<<"\nEmployee Name      :  "<<emp_name;
-	cout<<"\nEmployee number    :  "<<emp_number;
-	cout<<"\nBasic salary       :  "<<emp_basic;
-	cout<<"\nEmployee DA        :  "<<emp_da;
-	cout<<"\nIncome Tax         :  "<<emp_it;
-	cout<<"\nNet Salary         :  "<<find_net_salary(emp_basic, emp_da, emp_it);
+	cout<<"\n\n** Details of  Employee **";
+	cout<<"\nEmployee number       :  "<<emp_number;
+	cout<<"\nEmployee Name         :  "<<emp_name;
+	cout<<"\nEmployee dept name    :  "<<emp_dept_name;
+	cout<<"\nEmployee designation  :  "<<emp_designation;
 	cout<<"\n-------------------------------\n\n";
 }
 
