2066ccec0c35cfb3af9e0e959353a2bfae601a0f
Mani7Sai <112700177+Mani7Sai@users.noreply.github.com>
2022-09-23 14:00:39 +0530

    Update D3 3.Piggy bank.cpp

--- D3 3.Piggy bank.cpp
+++ D3 3.Piggy bank.cpp
@@ -1,32 +1,28 @@
 #include <iostream>
-#include <string>
 using namespace std;
-class IntegerI{
-private:
-	int i;
+class AddAmount
+{
+    int amount;
 public:
-	IntegerI(int i){
-		this->i=i;
-	}
-	void printNum(){
-		cout<<"i = "<<i<<"\n";
-	}
+    AddAmount()
+    {
+        amount = 50;
+    }
+    AddAmount(int a)
+    {
+        amount = 50;
+        amount = a+amount;
+    }
+    void print_amount()
+    {
+        cout << amount << endl;
+    }
 };
-class IntegerJ:public IntegerI{
-private:
-	int j;
-public:
-	IntegerJ(int i,int j):IntegerI(i){
-		this->j=j;
-	}
-	void printNum(){
-		IntegerI::printNum();
-		cout<<"j = "<<j<<"\n";
-	}
-};
-int main() {
-	IntegerJ integerJ(5,6);
-	integerJ.printNum();
-	system("pause");
-	return 0;
+int main()
+{
+    AddAmount a1;
+    AddAmount a2(15);
+    a1.print_amount();
+    a2.print_amount();
+    return 0;
 }
